f438b881378f2a5cac480176a9292a290d3914d2
YuJiaLiang0702 <ch993115@gmail.com>
2024-10-18 04:00:59 +0800

    Initial Commit

---
+++ PTests/PTests.swift
@@ -0,0 +1,17 @@
+//
+//  PTests.swift
+//  PTests
+//
+//  Created by Jesse Liang on 2024/10/18.
+//
+
+import Testing
+@testable import P
+
+struct PTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
